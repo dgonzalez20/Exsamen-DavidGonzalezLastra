d3d3c3ec61b1098198491da165fbf4499d63552a
david <dgonzalezl@cifpfbmoll.eu>
2022-02-10 19:54:27 +0100

    Crear usuario

--- laravel_practica_test-master/ExsamenDavidGonzalezLastra/database/seeders/PostSedder.php
+++ laravel_practica_test-master/ExsamenDavidGonzalezLastra/database/seeders/PostSedder.php
@@ -13,6 +13,36 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(10)->create();
+         $user=user::create({
+             <?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        public function run() {
+            $user = User::create([
+                        'doi' => $this->faker->unique()->dni(),
+                        'name' => 'David',
+                        'apellidos' => 'Gonzalez',
+                        'fechalta' => "2022-02-10",
+                        'email' => 'dgonzalezl@cifpfbmoll.eu',
+                        'email_verified_at' => now(),
+                        'password' => Hash::make('secretos'),
+                        'remember_token' => Str::random(10),
+            ]);
+    }
+}
+
+         })
     }
 }
