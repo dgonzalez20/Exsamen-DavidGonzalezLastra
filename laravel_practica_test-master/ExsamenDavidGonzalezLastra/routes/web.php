75f052fbed767c8b6763b5a462c0fb31b838cef3
david <dgonzalezl@cifpfbmoll.eu>
2022-02-10 19:34:56 +0100

    Creo routa tipo resource, al controlador, que tiene métodos crud

--- laravel_practica_test-master/ExsamenDavidGonzalezLastra/routes/web.php
+++ laravel_practica_test-master/ExsamenDavidGonzalezLastra/routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -22,3 +23,5 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::resource('pOST', PostController::class);
